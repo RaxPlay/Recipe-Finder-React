f0eea10b4ea7ee2737a2eee2fe329b1c64c8d36c
Raxon Suri <raxonsuriperez@gmail.com>
2026-02-20 15:19:23 -0500

    Recipe Finder App project with React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './Styles/App.css'
+
+import React, { useState } from 'react'
+
+export const App = () => {
+  return (
+    <>
+      <header>
+        <h1>Recipe-Finder</h1>
+      </header>
+    </>
+  )
+}
